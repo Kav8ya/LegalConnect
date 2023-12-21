e168e8eccc1da5d654e76fd41cb325da13b49f8f
Sri Dharani R <136625923+SriDharani26@users.noreply.github.com>
2023-12-21 22:25:54 +0530

    Update lawyer_login.php

--- lawyer_login.php
+++ lawyer_login.php
@@ -44,7 +44,7 @@ if (isset($_POST['submit'])) {
     <title>Login Form</title>
 
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <style>
         body {
             background-image: url("bgimg.png");
